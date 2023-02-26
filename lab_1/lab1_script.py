ee98212c42e8a06085ebbee55d2c9c7486c77a72
AlexZhagorov <zhagorov2004@gmail.com>
2023-02-26 14:32:55 +0300

    .even numbers added

--- lab_1/lab1_script.py
+++ lab_1/lab1_script.py
@@ -2,6 +2,7 @@ ADD_ACTION = 'add'
 SUB_ACTION = 'sub'
 MUL_ACTION = 'mul'
 DEV_ACTION = 'dev'
+NUM_LIST = [1,2,3,4,5,6,7,8,9,10]
 
 print('Hello, World!')
 
@@ -22,3 +23,11 @@ def action_func (f_num, s_num, action):
         return 'Incorrect action!'
 
 print(action_func(a,b,act))
+
+result_list = list()
+
+for num in NUM_LIST:
+    if (num % 2 == 0):
+        result_list.append(num)
+
+print(result_list)
