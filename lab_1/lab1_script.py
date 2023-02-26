570a757e1855608c0ebb81babf2a079fddd57159
AlexZhagorov <zhagorov2004@gmail.com>
2023-02-26 14:12:12 +0300

    .action_func added

--- lab_1/lab1_script.py
+++ lab_1/lab1_script.py
@@ -1,1 +1,24 @@
+ADD_ACTION = 'add'
+SUB_ACTION = 'sub'
+MUL_ACTION = 'mul'
+DEV_ACTION = 'dev'
+
 print('Hello, World!')
+
+a = input('Enter the first number: ')
+b = input('Enter the second number: ')
+act = input('Enter the action: ')
+
+def action_func (f_num, s_num, action):
+    if (action == ADD_ACTION):
+        return f'Result is {float(f_num) + float(s_num)}'
+    elif (action == SUB_ACTION):
+        return f'Result is {float(f_num) - float(s_num)}'
+    elif (action == MUL_ACTION):
+        return f'Result is {float(f_num) * float(s_num)}'
+    elif (action == DEV_ACTION):
+        return f'Result is {float(f_num) / float(s_num)}'
+    else:
+        return 'Incorrect action!'
+
+print(action_func(a,b,act))
